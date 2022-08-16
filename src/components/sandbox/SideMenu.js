170982fe474b7612869e62646eef4312d7a58bc4
rin <785879986@qq.com>
2022-08-16 17:02:51 +0800

    动态SideMenu.js 组件

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,53 +1,49 @@
 import React, { useState } from 'react';
 import { Layout, Menu } from 'antd';
 import {
-	UploadOutlined,
+	UnlockOutlined,
 	UserOutlined,
-	VideoCameraOutlined,
-	AppstoreOutlined,
-	ContainerOutlined,
-	DesktopOutlined,
-	MailOutlined,
-	MenuFoldOutlined,
-	MenuUnfoldOutlined,
-	PieChartOutlined,
+	HomeOutlined,
 } from '@ant-design/icons';
 import './index.css'
+import { withRouter } from 'react-router-dom'
 
 const { Sider } = Layout;
 
 
-function getItem(label, key, icon, children, type) {
-	return {
-		key,
-		icon,
-		children,
-		label,
-		type,
-	};
-}
-
-const items = [
-	getItem('首页', '/home', <PieChartOutlined />),
-	getItem('用户管理', '/user-manage', <DesktopOutlined />, [
-		getItem('用户列表', '/user-manage/list')
-	]),
-	getItem('权限管理', '/right-manage', <MailOutlined />, [
-		getItem('角色列表', '/right-manage/role/list'),
-		getItem('权限列表', '/right-manage/right/list'),
-	])
-];
-
-
 function SideMenu(props) {
+
 	const [collapsed] = useState(false);
+
+	function getItem(label, key, icon, children, type) {
+		return {
+			key,
+			icon,
+			children,
+			label,
+			type,
+		};
+	}
+
+	const items = [
+		getItem('首页', '/home', <HomeOutlined />),
+		getItem('用户管理', '/user-manage', <UserOutlined />, [
+			getItem('用户列表', '/user-manage/list')
+		]),
+		getItem('权限管理', '/right-manage', <UnlockOutlined />, [
+			getItem('角色列表', '/right-manage/role/list'),
+			getItem('权限列表', '/right-manage/right/list'),
+		])
+	];
+
 	return (
 		<Sider trigger={null} collapsible collapsed={collapsed}>
 			<div className="logo">全球新闻发布管理系统</div>
-			<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items} />
-
+			<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items} onClick={(obj) => {
+				props.history.push(obj.key)
+			}} />
 		</Sider>
 	);
 }
 
-export default SideMenu;
+export default withRouter(SideMenu);
