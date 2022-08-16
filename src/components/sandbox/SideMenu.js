2a0e2bf31d31393b2f76122bb036fa682695cd18
rin <785879986@qq.com>
2022-08-16 16:33:39 +0800

    SideMenu.js 组件

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -4,37 +4,48 @@ import {
 	UploadOutlined,
 	UserOutlined,
 	VideoCameraOutlined,
+	AppstoreOutlined,
+	ContainerOutlined,
+	DesktopOutlined,
+	MailOutlined,
+	MenuFoldOutlined,
+	MenuUnfoldOutlined,
+	PieChartOutlined,
 } from '@ant-design/icons';
+import './index.css'
 
 const { Sider } = Layout;
+
+
+function getItem(label, key, icon, children, type) {
+	return {
+		key,
+		icon,
+		children,
+		label,
+		type,
+	};
+}
+
+const items = [
+	getItem('首页', '/home', <PieChartOutlined />),
+	getItem('用户管理', '/user-manage', <DesktopOutlined />, [
+		getItem('用户列表', '/user-manage/list')
+	]),
+	getItem('权限管理', '/right-manage', <MailOutlined />, [
+		getItem('角色列表', '/right-manage/role/list'),
+		getItem('权限列表', '/right-manage/right/list'),
+	])
+];
+
 
 function SideMenu(props) {
 	const [collapsed] = useState(false);
 	return (
 		<Sider trigger={null} collapsible collapsed={collapsed}>
-			<div className="logo" />
-			<Menu
-				theme="dark"
-				mode="inline"
-				defaultSelectedKeys={['1']}
-				items={[
-					{
-						key: '1',
-						icon: <UserOutlined />,
-						label: 'nav 1',
-					},
-					{
-						key: '2',
-						icon: <VideoCameraOutlined />,
-						label: 'nav 2',
-					},
-					{
-						key: '3',
-						icon: <UploadOutlined />,
-						label: 'nav 3',
-					},
-				]}
-			/>
+			<div className="logo">全球新闻发布管理系统</div>
+			<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items} />
+
 		</Sider>
 	);
 }
