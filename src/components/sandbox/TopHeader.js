b6e7c9edff627192d8820befe7caccc8fceae1a6
rin <785879986@qq.com>
2022-08-16 10:15:39 +0800

    搭建路由组件

---
+++ src/components/sandbox/TopHeader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TopHeader(props) {
+	return (
+		<div>TopHeader</div>
+	);
+}
+
+export default TopHeader;
--- src/view/sandbox/NewSandBox.js
+++ src/view/sandbox/NewSandBox.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import SideMenu from '../../components/sandbox/SideMenu'
+import TopHeader from '../../components/sandbox/TopHeader'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import Home from './home/Home'
+import UserList from './user-manage/UserList'
+import RoleList from './right-manage/RoleList'
+import RightList from './right-manage/RightList'
+import NoPermission from './nopermission/NoPermission'
 
 function NewSandBox(props) {
 	return (
-		<div>NewSandBox</div>
+		<div>
+			<SideMenu></SideMenu>
+			<TopHeader></TopHeader>
+			<Switch>
+				<Route path="/home" component={Home}></Route>
+				<Route path="/user-manage/list" component={UserList}></Route>
+				<Route path="/right-manage/role/list" component={RoleList}></Route>
+				<Route path="/right-manage/right/list" component={RightList}></Route>
+				<Redirect from="/" to="/home" exact />
+				<Route path="*" component={NoPermission} />
+			</Switch>
+		</div>
 	);
 }
 
