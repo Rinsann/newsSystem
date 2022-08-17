79c3c278c3a008b89ef6337b035686dbac5fb112
rin <785879986@qq.com>
2022-08-17 12:32:58 +0800

    JSONServer mock数据

--- src/view/sandbox/home/Home.js
+++ src/view/sandbox/home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Button } from 'antd'
-import axios from 'axios'
+// import axios from 'axios'
 
 function Home(props) {
 	const ajax = () => {
 		/* axios.get('http://localhost:9000/roles?id=1').then(res => {
 		 console.log(res.data)
 		 }) */
-		axios.post('http://localhost:9000/roles', {
+	/* 	axios.post('http://localhost:9000/roles', {
 			id: 7990,
 			username: '5dshufhdsui',
 			password: 12343356,
@@ -15,7 +15,12 @@ function Home(props) {
 			default: true,
 			region: '',
 			roleId: 7990
-		})
+		}).then(r => {
+			console.log(r)
+		}) */
+	//	put 修改时直接替换整个，不方便修改部分字段
+	//	patch 更新补丁不受影响的字段不修改
+	//	删除 delete
 	}
 	return (
 		<div>
